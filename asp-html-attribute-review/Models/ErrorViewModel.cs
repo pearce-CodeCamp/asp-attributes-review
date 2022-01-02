33971e9f6bb326e0d6a1e9a143cc52a400848f06
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-01-02 17:45:20 -0600

    Initial commit:

---
+++ asp-html-attribute-review/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp_html_attribute_review.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
