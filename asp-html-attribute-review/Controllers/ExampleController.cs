78dcb9e07710b20335e521ac6f3e88471670341b
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-01-02 18:42:10 -0600

    Created Example controller and example views, handled form and a tag stuff

---
+++ asp-html-attribute-review/Controllers/ExampleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_html_attribute_review.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // this method handles GET requests made at /example/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // routed to GET requests at /example/data
+        // no need to specify http method and route when the method name matches the route name
+        // [HttpGet]
+        // [Route("/data")]
+        public IActionResult Data(string foo)
+        {
+            ViewBag.foo = foo;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormHandler(string name)
+        {
+
+            ViewBag.name = name;
+
+            // have this FormHandler rerender /example/index
+            return View("Index");
+        }
+    }
+}
